08e127547ffb41a70a5b40fbb8d6b6a17aef0f87
alessandrogama <alessandroolavogama@gmail.com>
2026-04-11 20:59:23 -0300

    feat: Created Repository for Asset

---
+++ src/main/java/com/assettrack/assettrack_api/domain/repository/AssetRepository.java
@@ -0,0 +1,17 @@
+package com.assettrack.assettrack_api.domain.repository;
+
+import com.assettrack.assettrack_api.domain.entity.Asset;
+import com.assettrack.assettrack_api.domain.valueobject.AssetStatus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AssetRepository {
+    Asset save(Asset asset);
+    Optional<Asset> findById(UUID id);
+    List<Asset> findAll();
+    List<Asset> findAllByStatus(AssetStatus status);
+    boolean existsById(UUID id);
+    void deleteById(UUID id);
+}
