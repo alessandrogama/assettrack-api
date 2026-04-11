3dba5aba8c99ccd1eee0a8fabae90756ac953df0
alessandrogama <alessandroolavogama@gmail.com>
2026-04-11 11:44:49 -0300

    feat: Initialize Spring Boot project

---
+++ src/main/java/com/assettrack/assettrack_api/AssettrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.assettrack.assettrack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssettrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssettrackApiApplication.class, args);
+	}
+
+}
